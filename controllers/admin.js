fd8d8d40a11aa5863b53c9ca830d1b80e2f1ce71
Andrew <andrew.cathcart@salecycle.com>
2018-12-02 23:57:45 +0000

    auth implemented to display products only created by the user

--- controllers/admin.js
+++ controllers/admin.js
@@ -76,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({ userId: req.user._id })
         // .select('title price -_id')
         // .populate('userId', 'name')
         .then(products => {
