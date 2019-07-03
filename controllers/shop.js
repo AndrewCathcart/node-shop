bcc0f31f44d8856dcd0669c5d9ae7271c2fadfaf
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-03 23:35:52 +0100

    added checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -130,6 +130,30 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,7 @@ router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", isAuth, shopController.getCart);
 router.post("/cart", isAuth, shopController.postCart);
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
+router.get("/checkout", isAuth, shopController.getCheckout);
 router.post("/create-order", isAuth, shopController.postOrder);
 router.get("/orders", isAuth, shopController.getOrders);
 router.get("/orders/:orderId", isAuth, shopController.getInvoice);
