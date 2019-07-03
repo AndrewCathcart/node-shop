b18c7c7b9e2cc12748b8a509001cf873634b0679
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-03 14:34:41 +0100

    fixed error handling

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -57,12 +63,6 @@ app.use((req, res, next) => {
     });
 });
 
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
 app.use(authRoutes);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -72,7 +72,12 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect("/500");
+  //
+  res.status(500).render("500", {
+    pageTitle: "Error",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
